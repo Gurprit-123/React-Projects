ea281e2d4e0403479d32680be73b4d85243b0abf
Gurprit singh <gurpritsingh7050@gmail.com>
2024-04-05 21:16:04 +0530

    Myntra Project Added

---
+++ MyntraProject/myntra-react-clone/src/main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./routes/App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Bags from "./routes/Bags.jsx";
+import Home from "./routes/Home.jsx";
+import { Provider } from "react-redux";
+import myntraStore from "./store/index.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/bag",
+        element: <Bags />,
+        /*action: createPostAction,*/
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={myntraStore}>
+      <RouterProvider router={router} />
+    </Provider>
+    {/* <App /> */}
+  </React.StrictMode>
+);
---
+++ MyntraProject/myntra-react-clone/src/store/index.js
@@ -0,0 +1,19 @@
+import {configureStore} from '@reduxjs/toolkit'
+import itemsSlice from './ItemsSlice'
+import fetchStatusSlice from './fetchStatusSlice'
+import bagsSlice from './bagsSlice'
+
+
+
+
+
+
+const myntraStore=configureStore({reducer:{
+items:itemsSlice.reducer,
+fetchStatus:fetchStatusSlice.reducer,
+bags:bagsSlice.reducer
+}})
+
+
+
+export default myntraStore
